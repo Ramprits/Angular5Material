25a35375e5281984ad65b1eccd3cbe5ba0886a75
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-12-08 17:43:54 +0530

    initial update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./ui/dashboard/dashboard.component";
+import { ContactComponent } from "./ui/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "dashboard" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "contact", component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { AppComponent } from "./app.component";
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
-
-import { environment } from '../environments/environment';
-
+import { environment } from "../environments/environment";
+import { DashboardComponent } from "./ui/dashboard/dashboard.component";
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatToolbarModule
+} from "@angular/material";
+import { NavComponent } from "./ui/nav/nav.component";
+import { ContactComponent } from "./ui/contact/contact.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavComponent,
+    ContactComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
+    FlexLayoutModule,
+    environment.production
+      ? ServiceWorkerModule.register("/ngsw-worker.js")
+      : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
